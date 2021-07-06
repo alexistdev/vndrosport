d19bbf5737ff15905025ab41ef1703e878858220
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2021-07-06 07:56:56 +0700

    Desain login android

---
+++ Android/vndrosport/app/src/main/java/com/gilang/vndrosport/page/login.java
@@ -0,0 +1,16 @@
+package com.gilang.vndrosport.page;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gilang.vndrosport.R;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
